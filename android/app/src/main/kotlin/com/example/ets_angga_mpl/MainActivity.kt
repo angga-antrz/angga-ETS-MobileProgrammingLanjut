2ba68d5f593f54d35bd51736cd18597c7bb16d9a
Angga Antareza <angga20123002@digitechuniversity.ac.id>
2026-05-08 23:08:56 +0700

    native berhasil

--- android/app/src/main/kotlin/com/example/ets_angga_mpl/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ets_angga_mpl/MainActivity.kt
@@ -1,5 +1,55 @@
 package com.example.ets_angga_mpl
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.widget.Toast
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "utd.ac.id/native_jembatan"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Baterai tidak terbaca.", null)
+                }
+            } else if (call.method == "showToast") {
+                val pesan = call.argument<String>("message")
+                Toast.makeText(this, pesan, Toast.LENGTH_SHORT).show()
+                result.success(true)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent?.let {
+                val level = it.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+                val scale = it.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+                if (level == -1 || scale == -1) -1 else (level * 100 / scale)
+            } ?: -1
+        }
+        return batteryLevel
+    }
+}
